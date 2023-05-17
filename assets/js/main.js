7668360859986f4e8ab3e2fd5aa71f720406521e
rogelocard <rsca@misena.edu.co>
2023-05-17 15:28:28 -0500

    Added mockup website structure in HTML, CSS and JS, first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/ 
