c2a5081375e3557dd884b04f6b3178c94a9b5780
JuanVR <juanvr@arqs.co>
2023-05-20 15:55:04 -0500

    subo desarrollo pantalla de habilodades

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*==================== REMOVE MENU MOBILE ====================*/
 
-
 /*==================== ACCORDION SKILLS ====================*/
+function updateProgressBar(elementId, percent) {
+  var progressBar = document.getElementById(elementId);
+  progressBar.style.width = percent + "%";
+}
 
+// Actualiza los valores de las barras de progreso
+updateProgressBar("html-progress", 80);
+updateProgressBar("css-progress", 70);
+updateProgressBar("js-progress", 60);
+updateProgressBar("sql-progress", 70);
+updateProgressBar("php-progress", 90);
+updateProgressBar("english-progress", 60);
 
 /*==================== QUALIFICATION TABS ====================*/
-
 
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
